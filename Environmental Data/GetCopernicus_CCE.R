8241eb25c5981887d19bc38515f9072663e63581
tsharan11 <tsharan@ucsd.edu>
2025-07-01 12:49:58 -0700

    Environmental Variables
    started accessing environmental data

--- Environmental Data/GetCopernicus_CCE.R
+++ Environmental Data/GetCopernicus_CCE.R
@@ -1,33 +1,91 @@
-library(ncdf4)
 library(lubridate)
 library(CopernicusMarine)
 library(stars)
+library(ncdf4) # package for netcdf manipulation
+#library(RNetCDF)
+library(raster) # package for raster manipulation
+library(tidyverse)
 
-CopernicusMarine_uid = "nposdaljian"
-CopernicusMarine_pwd = "Phd0c3an0graphy!"
+# -----------Step 1: Access Copernicus data-----------
+# Was getting 404 error when trying to download data (even though I changed function to match the updated CopernicusMarine library)
+# Downloaded Copernicus data directly from the website
+# Variables (daily resolution): east water velocity, north water velocity, SSH, mixed layer thickness, salinity
 
-YEARS = c(2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
+# YEARS = c(2014, 2015, 2016)
+# 
+# for (i in 1:length(YEARS)){
+#   year = YEARS[i]
+#   out_dir = paste("C:/Users/HARP/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data",as.character(year),".nc",sep = "")
+#   timerange1 = paste(as.character(year),"-01-01",sep="")
+#   timerange2 = paste(as.character(year),"-12-31",sep="")
+#   cms_download_subset(
+#     username = getOption("CopernicusMarine_uid", "tsharan"),
+#     password = getOption("CopernicusMarine_pwd", "H!ghfr3qu3ncy"),
+#     destination = out_dir,
+#     product = "GLOBAL_MULTIYEAR_PHY_001_030", # changed from GLOBAL_REANALYSIS_PHY_001_031
+#     layer = "cmems_mod_glo_phy_my_0.083deg_P1D-m_202311/zos",
+#     variable = "zos",
+#     # output = "netcdf",
+#     region = c(-59, -62, -52, -60), # bounding box 52 t0 59 W & 60 to 62 S
+#     timerange = c(timerange1,timerange2),
+#   # sub_variables = c("mlotst_mean","so_mean","thetao_mean","uo_mean",
+#   # "vo_mean","zos_mean"),
+#   # sub_variables = c("mlotst_mean","mlotst_std","so_mean","so_std","thetao_mean","thetao_std","uo_mean",
+#   #                   "uo_std","vo_mean","vo_std","zos_mean","zos_std"),
+# #   #verticalrange = c(0.5057600140571594,1.56)
+#   #verticalrange = c(0,-2)
+# )
+# }
 
-for (i in 1:length(YEARS)){
-  year = YEARS[i]
-  out_dir = paste("L:/My Drive/FourLoko/HabitatVariables/CCE_",as.character(year),".nc",sep = "")
-  timerange1 = paste(as.character(year),"-01-01",sep="")
-  timerange2 = paste(as.character(year),"-12-31",sep="")
+# -----------------Step 2: Construct Timeseries-------------------
+# Load Copernicus data
+EI_cop <- nc_open("C:/Users/HARP/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data/Copernicus/EI_cmems_mod_glo_phy_my_0.083deg_P1D-m_1751320063762.nc")
+KGI_cop <- nc_open("C:/Users/HARP/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data/Copernicus/KGI_cmems_mod_glo_phy_my_0.083deg_P1D-m_1751320609977.nc")
+CI_cop <- nc_open("C:/Users/HARP/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data/Copernicus/CI_cmems_mod_glo_phy_my_0.083deg_P1D-m_1751320549137.nc")
 
-copernicus_download_motu(
-  username = CopernicusMarine_uid,
-  password = CopernicusMarine_pwd,
-  destination = out_dir,
-  product = "GLOBAL_REANALYSIS_PHY_001_031",
-  layer = "global-reanalysis-phy-001-031-grepv2-mnstd-daily",
-  variable = "sea_surface_height",
-  output = "netcdf",
-  region = c(-130, 28, -113, 48),
-  timerange = c(timerange1,timerange2),
-  sub_variables = c("mlotst_mean","so_mean","thetao_mean","uo_mean",
-  "vo_mean","zos_mean"),
-  # sub_variables = c("mlotst_mean","mlotst_std","so_mean","so_std","thetao_mean","thetao_std","uo_mean",
-  #                   "uo_std","vo_mean","vo_std","zos_mean","zos_std"),
-  verticalrange = c(0.5057600140571594,1.56)
-)
+dfFromNC <- function(data) {
+  # Extracting relevant data
+  time <- ncvar_get(data, 'time')
+  lat <- ncvar_get(data, 'latitude')
+  lon <- ncvar_get(data, 'longitude')
+  ssh <- as.vector(ncvar_get(data, 'zos'))
+  n_velocity <- as.vector(ncvar_get(data, 'vo'))
+  e_velocity <- as.vector(ncvar_get(data, 'uo'))
+  salinity <- as.vector(ncvar_get(data, 'so'))
+  mixed_layer <- as.vector(ncvar_get(data, 'mlotst'))
+  
+  # Cleaning up latitude, longitude, and time variables
+  time_obs <- as.POSIXct(time, origin = "1970-01-01", tz="UTC") # Changing from seconds to time
+  lonlattime <- as.matrix(expand.grid(lon,lat,time_obs))
+  
+  # Creating a dataframe with all variables
+  lonlat <- expand.grid(lon = lon, lat = lat, time = time_obs)
+  
+  df <- data.frame(
+    long = lonlat$lon,
+    lat = lonlat$lat,
+    date = lonlat$time,
+    ssh = as.vector(ssh),
+    n_velocity = as.vector(n_velocity),
+    e_velocity = as.vector(e_velocity),
+    salinity = as.vector(salinity),
+    mixed_layer = as.vector(mixed_layer)
+  )
+  # df <- data.frame(cbind(lonlattime, ssh, n_velocity, e_velocity, salinity, mixed_layer))
+  # colnames(df) <- c("long","lat","date","ssh", "n_velocity",
+  #                         "e_velocity", "salinity", "mixed_layer")
+
+  # Creating a dataframe with daily spatial averages
+  avg_df <- df %>% group_by(date) %>% summarize(ssh = mean(ssh), n_velocity = mean(n_velocity),
+                                                e_velocity = mean(e_velocity), salinity = mean(salinity),
+                                                mixed_layer = mean(mixed_layer))
+
+  return(avg_df)
+  }
+dfFromNC(EI_cop)
+KGI_df <- dfFromNC(KGI_cop)
+CI_df <- dfFromNC(CI_cop)
+
+CopTimeseries <- function(data) {
+  
 }
--- Environmental Data/GetERDDAP_Chla.R
+++ Environmental Data/GetERDDAP_Chla.R
@@ -1,6 +1,6 @@
 #This script queries the ERDDAP database for data
 #Learn more about the database here: https://coastwatch.pfeg.noaa.gov/erddap/index.html
-#NP 07/09/2021
+#TS 06/30/2025
 #Uses data from this link - https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdMH1chla1day_Lon0360.html
 #Load packages
 library(rerddap)
@@ -12,39 +12,38 @@ library(gganimate)
 library(ggplot2)
 library(plotdap)
 
-saveDir = paste("L:/My Drive/FourLoko/HabitatVariables/",sep="")
-# Regions = c('ENP','CCE','CentralPac')
-Regions = c('CentralPac')
+saveDir = paste("C:/Users/HARP/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data",sep="")
 AQUA_sstInfo <- info('erdMH1chla1day_Lon0360')
 
-for (i in 1:length(Regions)){
-  Region = Regions[i]
 
-  #Specify Lat and Long and time
-  if (Region == 'CCE'){
-    latitude = c(48.,28.)
-    longitude = c(230., 247)
-    years = c(2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022)}
-  
-  if (Region == 'ENP'){
-    latitude = c(60.,50.)
-    longitude = c(170., 220.)
-    years = c(2010,2011,2012,2013,2014,2015,2016,2017,2018,2019)}
-  
-  if (Region == 'CentralPac'){
-    latitude = c(30.,0.)
-    longitude = c(145., 216)
-    years = c(2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019)}
-  
-for (yy in 1:length(years)){
-  year = as.character(years[yy])
-  if (Region == 'CCE' & year == "2022"){
-    time = c(paste(year,"-01-01",sep=""),paste(year,"-07-27",sep=""))  
-  }else{
-    time = c(paste(year,"-01-01",sep=""),paste(year,"-12-31",sep=""))}
-  AQUASST <- griddap(AQUA_sstInfo, latitude = latitude, longitude = longitude, 
+GetChla <- function(site) {
+  # Specify Lat and Long and time
+  # Doing +/- 0.5 degree for now, can change if something else is better
+  if (site == 'EI'){
+    # site lat-long: -60.8869, -55.95400
+    latitude = c(-59.3869,-61.3869)
+    longitude = c(303.406, 304.406)
+    years = c(2014)
+    time = c(paste(years,"-03-05",sep=""),paste(years,"-07-17",sep="")) }
+  if (site == 'KGI'){
+    # site lat-long: -61.457817, -57.941917
+    latitude = c(-60.957817,-61.957817)
+    longitude = c(302.558083, 303.558083)
+    years = c(2015,2016)
+    time = c(paste(years[1],"-02-10",sep=""),paste(years[2],"-01-29",sep="")) }
+
+  if (site == 'CI'){
+    # site lat-long: -61.251867, -53.483433
+    latitude = c(-60.751867,-61.751867)
+    longitude = c(306.016567, 307.016567)
+    years = c(2016)
+    time = c(paste(years,"-02-04",sep=""),paste(years,"-12-02",sep="")) }
+  year_str <- paste(years, collapse = "-")
+  AQUASST <- griddap(AQUA_sstInfo, latitude = latitude, longitude = longitude,
                      time = time, fields = 'chlorophyll',
-                     store = disk(path = paste(saveDir,Region,"_ChlA_",year,".nc",sep =""), overwrite = TRUE))
-  
+                     store = disk(path = paste(saveDir, "/",site,"_ChlA_",year_str,".nc",sep =""), overwrite = TRUE))
+
 }
-}
+GetChla('EI')
+GetChla('KGI')
+GetChla('CI')
