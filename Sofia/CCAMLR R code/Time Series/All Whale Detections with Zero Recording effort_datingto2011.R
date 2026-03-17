b4bf22cec6411f4a867c9a7af1d979fa385a9ef1
nposdalj <36055755+nposdalj@users.noreply.github.com>
2026-03-17 11:35:09 -0700

    Update All Whale Detections with Zero Recording effort_datingto2011.R

--- Sofia/CCAMLR R code/Time Series/All Whale Detections with Zero Recording effort_datingto2011.R
+++ Sofia/CCAMLR R code/Time Series/All Whale Detections with Zero Recording effort_datingto2011.R
@@ -9,10 +9,14 @@ library(units)
 # ------------------------------
 # LOAD DATA
 # ------------------------------
-data <- read.csv("C:/Users/nposd/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data/Copernicus/Deseasoning/KGI_deseasoned.csv")
+env_file   <- "C:/Users/HARP/Documents/GitHub/antarctic-odontocete-habitat/Environmental Data/Copernicus/Deseasoning/CI_deseasoned.csv"
+catch_file <- "D:/CCAMLR Data/756/R/756_USA_2026-01-02.Rds"
+
+env_var <- "salinity_0"   # temperature_0, salinity_0 , chla_0, productivity_0, mlayer_0
+
 
 #data_list <- readRDS("D:/CCAMLR Data/756/R/756_USA_2026-01-02.Rds")
-data_list <- readRDS("F:/Antarctica/756_USA_2026-01-02.Rds")
+data_list <- readRDS("D:/CCAMLR Data/756/R/756_USA_2026-01-02.Rds")
 C1        <- data_list[["C1"]]
 C1_CATCH  <- data_list[["C1_CATCH"]]
 
@@ -20,6 +24,7 @@ C1_CATCH  <- data_list[["C1_CATCH"]]
 # SPATIAL BUFFERS FOR KRILL CATCH
 # ------------------------------
 buffer_km     <- 100
+
 buffer_m      <- set_units(buffer_km, "km") |> set_units("m")
 antarctic_crs <- 3031
 
@@ -92,3 +97,68 @@ plot(daily_combined$value,daily_combined$temperature_0)
 abline(lm(daily_combined$temperature_0 ~ daily_combined$value), col = "red")
 
 # To find p-value you'll have to fit a linear regression model
+
+
+model_data <- daily_combined %>%
+  filter(!is.na(value), !is.na(temperature_0))
+
+
+model <- lm(temperature_0 ~ value, data = model_data)
+summary(model)
+
+r2   <- summary(model)$r.squared
+pval <- summary(model)$coefficients[2,4]
+
+
+ggplot(model_data, aes(x = value, y = temperature_0)) +
+  
+ 
+  geom_point(
+    size = 2.2,
+    color = "black",
+    alpha = 1
+  ) +
+  
+ 
+  geom_smooth(
+    method = "lm",
+    color = "black",
+    linewidth = 1.2,
+    se = TRUE,
+    fill = "grey",
+    alpha = 0.2
+  ) +
+  
+  labs(
+    x = "Krill Catch (kg)",
+    y = "Sea Surface Temperature",
+    title = "Krill Catch vs Temperature"
+  ) +
+  
+
+  annotate(
+    "label",
+    x = Inf, y = Inf,
+    label = paste0("R² = ", round(r2, 3),
+                   "\np = ", signif(pval, 3)),
+    hjust = 1.1, vjust = 1.3,
+    size = 5,
+    label.size = 0.5,
+    fill = "white",
+    color = "black"
+  ) +
+  
+  
+  theme_classic(base_size = 14) +
+  theme(
+    plot.title = element_text(face = "bold", hjust = 0.5),
+    axis.text  = element_text(color = "black"),
+    axis.title = element_text(face = "bold"),
+    
+    panel.grid.major = element_line(color = "grey80", linewidth = 0.5),
+    panel.grid.minor = element_line(color = "grey90", linewidth = 0.3),
+    
+    axis.line  = element_line(linewidth = 0.8),
+    axis.ticks = element_line(linewidth = 0.8)
+  )
+
